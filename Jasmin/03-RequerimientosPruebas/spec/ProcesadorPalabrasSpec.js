a146299271a4dd6117c3d8aded1b88f8325a9ed3
luis <lomejordejr@riseup.net>
2017-01-15 14:07:30 -0500

    Agregado requerimientosPruebas

---
+++ Jasmin/03-RequerimientosPruebas/spec/ProcesadorPalabrasSpec.js
@@ -0,0 +1,14 @@
+//Describir
+describe("Procesador de palabras dado una frase", function () {
+  it("debe eliminar los espacios en blanco", function () {
+    expect(limpiezaEspacios("calidad de software")).toEqual("calidaddesoftware")
+  });
+
+  it("debe eliminar las vocales", function () {
+    expect(limpiezaVocales("calidad de software")).toEqual("cldd d sftwar")
+  });
+
+  it("debe eliminar todas las consonantes", function () {
+    expect(limpiezaConsonantes("calidad de software")).toEqual("cldd d sftwar")
+  });
+})
