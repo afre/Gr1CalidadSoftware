86675a18805065f0a41c0ef8b0d39c1aea6d1e39
luis <lomejordejr@riseup.net>
2017-01-15 14:23:16 -0500

    Agregado requerimientosCodificacion

---
+++ Jasmin/04-RequerimientosCodificion/src/ProcesadorPalabras.js
@@ -0,0 +1,11 @@
+function limpiezaEspacios(frase) {
+  return "calidaddesoftware";
+}
+
+function limpiezaVocales(frase) {
+  return "cldd d sftwar";
+}
+
+function limpiezaConsonantes(frase) {
+  return "ia e oae";
+}
